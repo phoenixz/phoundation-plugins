c735058b8f3fbe239385b550acb1ba9272655b30
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-06-05 16:37:54 -0700

    Removed "Plugins" directory
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Sso/Library/Updates.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Updates class
+ *
+ * This is the Init class for the Accounts library
+ *
+ * @see       \Phoundation\Core\Libraries\Updates
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Accounts
+ */
+
+declare(strict_types=1);
+
+namespace Plugins\Phoundation\Sso\Library;
+
+class Updates extends \Phoundation\Core\Libraries\Updates
+{
+    /**
+     * The current version for this library
+     *
+     * @return string
+     */
+    public function version(): string
+    {
+        return '0.2.0';
+    }
+
+
+    /**
+     * The list of version updates available for this library
+     *
+     * @return void
+     */
+    public function updates(): void
+    {
+        $this->addUpdate('0.2.0', function () {
+            // Drop the tables to be sure we have a clean slate
+            sql()->schema()->table('phoundation_sso_signins')->drop();
+
+            // Create the phoundation_sso_signins table.
+            sql()->schema()->table('phoundation_sso_signins')->define()
+                 ->setColumns('
+                    `id` bigint NOT NULL AUTO_INCREMENT,
+                    `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                    `created_by` bigint DEFAULT NULL,
+                    `meta_id` bigint NULL DEFAULT NULL,
+                    `meta_state` varchar(16) CHARACTER SET latin1 DEFAULT NULL,
+                    `status` varchar(16) CHARACTER SET latin1 DEFAULT NULL,
+                    `users_id` bigint NOT NULL,
+                    `last_sign_in` datetime DEFAULT NULL,
+                    `sign_in_count` int NOT NULL,
+                ')->setIndices('                
+                    PRIMARY KEY (`id`),
+                    KEY `created_on` (`created_on`),
+                    KEY `created_by` (`created_by`),
+                    KEY `status` (`status`),
+                    KEY `meta_id` (`meta_id`),
+                    KEY `users_id` (`users_id`),
+                    KEY `last_sign_in` (`last_sign_in`),
+                    KEY `sign_in_count` (`sign_in_count`),
+                ')->setForeignKeys('
+                    CONSTRAINT `fk_phoundation_sso_signins_created_by` FOREIGN KEY (`created_by`) REFERENCES `accounts_users` (`id`) ON DELETE RESTRICT,
+                    CONSTRAINT `fk_phoundation_sso_signins_meta_id` FOREIGN KEY (`meta_id`) REFERENCES `meta` (`id`) ON DELETE CASCADE,
+                    CONSTRAINT `fk_phoundation_sso_signins_users_id` FOREIGN KEY (`users_id`) REFERENCES `accounts_users` (`id`) ON DELETE RESTRICT,
+                ')->create();
+        });
+    }
+}
---
+++ Phoundation/Sso/Saml.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Class Saml
+ *
+ *
+ *
+ * @see       https://github.com/SAML-Toolkits/php-saml
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Accounts
+ */
+
+declare(strict_types=1);
+
+namespace Plugins\Phoundation\Sso;
+
+use LightSaml\Credential\KeyHelper;
+use LightSaml\Credential\X509Certificate;
+use LightSaml\Model\Context\SerializationContext;
+use LightSaml\Model\Metadata\AssertionConsumerService;
+use LightSaml\Model\Metadata\EntityDescriptor;
+use LightSaml\Model\Metadata\KeyDescriptor;
+use LightSaml\Model\Metadata\SpSsoDescriptor;
+use LightSaml\Model\XmlDSig\SignatureWriter;
+use LightSaml\SamlConstants;
+use Phoundation\Accounts\Users\Interfaces\UserInterface;
+use Phoundation\Filesystem\Directory;
+use Phoundation\Filesystem\File;
+use Phoundation\Web\Http\UrlBuilder;
+use RobRichards\XMLSecLibs\XMLSecurityKey;
+
+class Saml
+{
+    /**
+     * @var UserInterface $user
+     */
+    protected UserInterface $user;
+
+
+    /**
+     * Saml class constructor
+     */
+    public function __construct()
+    {
+    }
+
+
+    public function test(): static
+    {
+        $entityDescriptor = new EntityDescriptor();
+        $entityDescriptor
+            ->setID(\LightSaml\Helper::generateID())
+            ->setEntityID('http://some.entity.id')
+        ;
+
+        $entityDescriptor->addItem(
+            $spSsoDescriptor = (new SpSsoDescriptor())->setWantAssertionsSigned(true)
+        );
+
+        $spSsoDescriptor->addKeyDescriptor(
+            $keyDescriptor = (new KeyDescriptor())
+                ->setUse(KeyDescriptor::USE_SIGNING)
+                ->setCertificate(X509Certificate::fromFile(File::new(DIRECTORY_ROOT . 'config/saml/saml-public-key.crt')->getPath()))
+        );
+
+        $spSsoDescriptor->addAssertionConsumerService(
+            $acs = (new AssertionConsumerService())
+                ->setBinding(SamlConstants::BINDING_SAML2_HTTP_POST)
+                ->setLocation(UrlBuilder::getWww('/sso/saml/acs.html'))
+        );
+
+        $context = new SerializationContext();
+showdie($entityDescriptor->serialize($context->getDocument(), $context));
+        return $this;
+    }
+
+    public function testCertificate(): static
+    {
+        $certificate = X509Certificate::fromFile(DIRECTORY_ROOT . 'config/saml/saml-public-key.crt');
+        $privateKey = KeyHelper::createPrivateKey(DIRECTORY_ROOT . 'config/saml/saml-public-key.pem', '', true, XMLSecurityKey::RSA_SHA256);
+
+        $message->setSignature(SignatureWriter::createByKeyAndCertificate($certificate, $privateKey));
+        $context = new SerializationContext();
+
+        showdie($message->serialize($context->getDocument(), $context));
+        return $this;
+    }
+}
