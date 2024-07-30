657c6e0ae8b2af12515ee232178a0dc10e7be4b0
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2024-07-30 13:07:11 -0600

    Updated Response object method names
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

--- Phoundation/Business/Library/pages/business/customer.php
+++ Phoundation/Business/Library/pages/business/customer.php
@@ -40,7 +40,7 @@ if (Request::isPostRequestMethod()) {
 
     } catch (ValidationFailedException $e) {
         // Oops! Show validation errors and remain on page
-        Response::getFlashMessages()->addMessage($e);
+        Response::getFlashMessagesObject()->addMessage($e);
         $customer->forceApply();
     }
 }
