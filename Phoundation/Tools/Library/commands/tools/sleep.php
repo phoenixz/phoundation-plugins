853b683404307950e0e4a17578f2301f33af2ee5
Sven Olaf Oostenbrink <sven@medinet.ca>
2024-12-16 16:53:10 -0800

    Added sleep command
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

---
+++ Phoundation/Tools/Library/commands/tools/sleep.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Command tools sleep
+ *
+ *
+ *
+ * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
+ * @copyright Copyright (c) 2022 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package Phoundation\Scripts
+ */
+
+
+declare(strict_types=1);
+
+use Phoundation\Cli\CliDocumentation;
+use Phoundation\Cli\CliCommand;
+use Phoundation\Data\Validator\ArgvValidator;
+use Phoundation\Utils\Strings;
+
+
+CliDocumentation::setUsage('./pho tools sleep 4
+./pho tools sleep 20');
+
+CliDocumentation::setHelp('The sleep tool script will sleep for the specified amount of seconds');
+
+
+// Get the arguments
+$argv = ArgvValidator::new()
+                     ->select('seconds')->isInteger()->isPositive()
+                     ->validate();
+
+
+// Sleep
+sleep($argv['seconds']);
