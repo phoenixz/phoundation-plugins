9a1c595d2e9d48f6da72d2d532fb6f72051a9bf0
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2024-12-28 16:33:53 -0800

    Added Torrents library
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/Torrents/Transmission/TransmissionDaemon.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class TransmissionDaemon
+ *
+ *
+ *
+ * @author    Sven Olaf Oostenbrink <sven@medinet.ca>
+ * @license   This plugin is developed by Medinet and may only be used by others with explicit written authorization
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Plugins\Phoundation\Torrents
+ */
+
+
+declare(strict_types=1);
+
+namespace Plugins\Phoundation\Torrents\Transmission;
+
+use Phoundation\Os\Processes\Commands\SystemCtl;
+
+
+class TransmissionDaemon extends SystemCtl
+{
+
+}
