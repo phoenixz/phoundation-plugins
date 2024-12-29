0fe08911be980b16e85d4c02ed2a3b0b27f7f11a
Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
2024-12-28 16:33:43 -0800

    Added Editors library
    Signed-off-by: Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>

---
+++ Phoundation/Editors/Editor.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Class Editor
+ *
+ *
+ *
+ * @author    Sven Olaf Oostenbrink <sven@medinet.ca>
+ * @license   This plugin is developed by Medinet and may only be used by others with explicit written authorization
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Plugins\Editors
+ */
+
+
+declare(strict_types=1);
+
+namespace Plugins\Phoundation\Editors;
+
+
+abstract class Editor
+{
+    public function install(): static
+    {
+
+    }
+
+
+    public function render(): ?string
+    {
+
+    }
+}
---
+++ Phoundation/Editors/SummerNote/SummerNote.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class SummerNote
+ *
+ *
+ * @see       https://summernote.org/
+ * @see       https://github.com/summernote/awesome-summernote
+ * @author    Sven Olaf Oostenbrink <sven@medinet.ca>
+ * @license   This plugin is developed by Medinet and may only be used by others with explicit written authorization
+ * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @package   Phoundation\Plugins\Editors
+ */
+
+
+declare(strict_types=1);
+
+namespace Plugins\Phoundation\Editors\SummerNote;
+
+use Plugins\Phoundation\Editors\Editor;
+
+class SummerNote extends Editor
+{
+
+}
