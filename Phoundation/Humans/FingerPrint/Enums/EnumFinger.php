8deb7e4fa09c8f1a17894e2184cc8784fa8fafbf
Sven Olaf Oostenbrink <sven@medinet.ca>
2025-01-23 16:58:20 -0800

    Updated copyright headers
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Humans/FingerPrint/Enums/EnumFinger.php
+++ Phoundation/Humans/FingerPrint/Enums/EnumFinger.php
@@ -7,7 +7,7 @@
  *
  * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license   http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
- * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @package   Plugins\Phoundation\FingerPrint
  */
 
@@ -24,7 +24,7 @@ namespace Plugins\Phoundation\Humans\FingerPrint\Enums;
  *
  * @author    Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @license http://opensource.org/licenses/GPL-2.0 GNU Public License, Version 2
- * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @package Plugins\Phoundation\Humans
  */
 enum EnumFinger: string
--- Phoundation/Torrents/Transmission/TransmissionDaemon.php
+++ Phoundation/Torrents/Transmission/TransmissionDaemon.php
@@ -7,7 +7,7 @@
  *
  * @author    Sven Olaf Oostenbrink <sven@medinet.ca>
  * @license   This plugin is developed by Medinet and may only be used by others with explicit written authorization
- * @copyright Copyright (c) 2024 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
+ * @copyright Copyright © 2025 Sven Olaf Oostenbrink <so.oostenbrink@gmail.com>
  * @package   Plugins\Phoundation\Torrents
  */
 
