1d5ff128c5b33b064352e74fb75bfd10dc17f646
Sven Olaf Oostenbrink <sven@medinet.ca>
2025-09-11 15:03:42 -0700

    Updated Phoundation method calls
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Hardware/Devices/Device.php
+++ Phoundation/Hardware/Devices/Device.php
@@ -382,7 +382,7 @@ class Device extends DataEntry implements DeviceInterface
                             ->setOptional(false)
                             ->setInputType(EnumInputType::text)
                             ->setSize(3)
-                            ->setDataSource([
+                            ->setSource([
                                 'scanner'   => tr('Scanner'),
                                 'printer'   => tr('Printer'),
                                 'webcam'    => tr('Webcam'),
