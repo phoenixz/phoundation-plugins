bcd198391619c82febb545875caba998db1b71f9
Harrison Macey <harrison@medinet.ca>
2025-09-22 16:11:31 -0700

    Improved Knowledgebase library
    Updated security/incidents page URL references
    
    Signed-off-by: Harrison Macey <harrison@medinet.ca>

--- Phoundation/Knowledgebase/Library/web/pages/knowledgebase/article.php
+++ Phoundation/Knowledgebase/Library/web/pages/knowledgebase/article.php
@@ -125,7 +125,7 @@ $o_relevant_card = Card::new()
                      ->setContent(($article->isNew() ? '' : AnchorBlock::new(Url::new('/profiles/profile+' . $article->getId() . '.html')->makeWww(), tr('Profile page for this article')) .
                                                             AnchorBlock::new(Url::new('/accounts/password+' . $article->getId() . '.html')->makeWww(), tr('Change password for this article')) .
                                                             AnchorBlock::new(Url::new('/security/authentications.html')->makeWww()->addQueries('articles_id=' . $article->getId()), tr('Authentications for this article')) .
-                                                            AnchorBlock::new(Url::new('/security/incidents.html')->makeWww()->addQueries('articles_id=' . $article->getId()), tr('Security incidents for this article'))) .
+                                                            AnchorBlock::new(Url::new('/reports/security/incidents.html')->makeWww()->addQueries('articles_id=' . $article->getId()), tr('Security incidents for this article'))) .
                                                             hr(AnchorBlock::new(Url::new('/accounts/roles.html')->makeWww(), tr('Roles management')) .
                                                                AnchorBlock::new(Url::new('/accounts/rights.html')->makeWww(), tr('Rights management'))));
 
