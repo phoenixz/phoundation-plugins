830434b8b7c32f9fe2382f778996fc8e3db5eef2
Sven Olaf Oostenbrink <sven@medinet.ca>
2026-01-26 14:17:20 -0800

    Improved Knowledgebase library
    Now uses new default buttons
    
    Signed-off-by: Sven Olaf Oostenbrink <sven@medinet.ca>

--- Phoundation/Knowledgebase/Library/web/pages/knowledgebase/articles.php
+++ Phoundation/Knowledgebase/Library/web/pages/knowledgebase/articles.php
@@ -29,51 +29,50 @@ use Phoundation\Web\Requests\Response;
 
 
 // Get the articles list and apply filters
-$articles = Articles::new();
-$builder  = $articles->getQueryBuilderObject()
-    ->addSelect(' `knowledgebase_articles`.`id`, 
-                  `knowledgebase_articles`.`name`, 
-                  `knowledgebase_articles`.`status`,')
-    ->addGroupBy('`knowledgebase_articles`.`id`');
+$o_articles = Articles::new();
+$o_builder  = $o_articles->getQueryBuilderObject()
+                         ->addSelect(' `knowledgebase_articles`.`id`, 
+                                       `knowledgebase_articles`.`name`, 
+                                       `knowledgebase_articles`.`status`,')
+                         ->addGroupBy('`knowledgebase_articles`.`id`');
 
 
 // Build "articles" table
-$buttons = Buttons::new()
-                  ->addButton(tr('Create'), EnumDisplayMode::primary, '/knowledgebase/article.html')
-                  ->addButton(tr('Delete'), EnumDisplayMode::warning, EnumButtonType::submit, true, true);
+$o_buttons = Buttons::new()
+                    ->addCreateButton(Url::new('/knowledgebase/article.html'))
+                    ->addDeleteButton(true);
 
 
 // TODO Automatically re-select items if possible
 //    ->select($post['id']);
-$articles_card = Card::new()
-    ->setTitle('Active articles')
-    ->setSwitches('reload')
-    ->setContent($articles->load()
-                          ->getHtmlDataTableObject([
-                              'name' => tr('Name'),
-                          ])
-                          ->setRowUrls('/knowledgebase/article+:ROW.html'))
-    ->useForm(true)
-    ->setButtonsObject($buttons);
+$o_articles_card = Card::new()
+                       ->setTitle('Active articles')
+                       ->setSwitches('reload')
+                       ->setContent($o_articles->load()
+                                               ->getHtmlDataTableObject([
+                                                   'name' => tr('Name'),
+                                               ])
+                                               ->setRowUrls('/knowledgebase/article+:ROW.html'))
+                       ->useForm(true)
+                       ->setButtonsObject($o_buttons);
 
-
-$articles_card->getForm()
-              ->setAction(Url::newCurrent())
-              ->setRequestMethod(EnumHttpRequestMethod::post);
+$o_articles_card->getFormObject()
+                ->setAction(Url::newCurrent())
+                ->setRequestMethod(EnumHttpRequestMethod::post);
 
 
 // Build relevant links
 $o_relevant_card = Card::new()
-                     ->setMode(EnumDisplayMode::info)
-                     ->setTitle(tr('Relevant links'))
-                     ->setContent(AnchorBlock::new(Url::new('/knowledgebase/articles.html')->makeWww(), tr('Knowledgebase')));
+                       ->setMode(EnumDisplayMode::info)
+                       ->setTitle(tr('Relevant links'))
+                       ->setContent(AnchorBlock::new(Url::new('/knowledgebase/articles.html')->makeWww(), tr('Knowledgebase')));
 
 
 // Build documentation
 $o_documentation_card = Card::new()
-                          ->setMode(EnumDisplayMode::info)
-                          ->setTitle(tr('Documentation'))
-                          ->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+                            ->setMode(EnumDisplayMode::info)
+                            ->setTitle(tr('Documentation'))
+                            ->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 
 // Set page meta-data
@@ -87,5 +86,5 @@ Response::setBreadcrumbs([
 
 // Render and return the page grid
 return Grid::new()
-           ->addGridColumn($articles_card                      , EnumDisplaySize::nine)
+           ->addGridColumn($o_articles_card                        , EnumDisplaySize::nine)
            ->addGridColumn($o_relevant_card . $o_documentation_card, EnumDisplaySize::three);
